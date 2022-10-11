26f4354f460566376cf00580fccd77d6a7b94db1
Abdessamad <80596901+Abdessamad-Y@users.noreply.github.com>
2022-10-11 20:09:13 +0000

    t

---
+++ Ecommerce/Ecommerce/celery.py
@@ -0,0 +1,7 @@
+import os
+from celery import Celery
+# set the default Django settings module for the 'celery' program.
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Ecommerce.settings')
+app = Celery('Ecommerce')
+app.config_from_object('django.conf:settings', namespace='CELERY')
+app.autodiscover_tasks()
